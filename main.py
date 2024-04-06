a0e5f1e9a142471fa211a99a2172459beaee64ea
Nicolas Beltran Castellanos <nicolas.bel.cas@gmail.com>
2024-04-06 18:39:50 -0500

    Se agregaron seccioanmiento de listas, y obtension de clave

--- main.py
+++ main.py
@@ -1,17 +1,32 @@
 import abecedario
 
 def run():
+    # Recibe el mensaje que desea encriptar.
+    mensaje = list(input('Escribe el mensaje: ').upper())
+    clave = list(input('Digita la clave de descifrado separado por comas: ').split(','))
     
-    mensaje = list(input("Escribe el mensaje: ").upper())
-    
-    def encriptador(mensaje):
+    def encriptador(mensaje,clave):
         numeracion =[]
+        
+    # Convertir las letras del mensaje a numeros
         for letra in mensaje:
-            valor = abecedario.abc[letra]
+            valor = abecedario.encriptacion[letra]
             numeracion.append(valor)
-        return numeracion
+            
+    # Agrupa los numeros en sub listas de 3 filas x n columnas
+        columnas = len(numeracion)
+        filas = 3
+        if columnas % 3!=0:
+            numeracion.append(0)
+            if columnas % 3!=0:
+                numeracion.append(0)
+
+    # Seccionador de lista a matriz deacuerdo a la cantidad de numeros
+        matriz = [numeracion[i:i + 3] for i in range(0,columnas,filas)]
+        clave = [clave[i:i + 3] for i in range(0,9,3)]
+        return  matriz, clave
     
-    print(encriptador(mensaje))
+    print(encriptador(mensaje,clave))
     
     
 
